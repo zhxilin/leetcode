5952da57da90da187cf12d1810323c1d3c72b694
zhxilin <zhxilin@live.com>
2020-06-26 17:13:35 +0800

    766. Toeplitz Matrix

---
+++ array/toeplitz-matrix.cpp
@@ -0,0 +1,24 @@
+//766. Toeplitz Matrix
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        for (int i = 0; i < m - 1; ++i) {
+            for (int j = 0; j < n - 1; ++j) {
+                if (matrix[i][j] != matrix[i + 1][j + 1])
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+};
+
+static auto _ =[]() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+    return NULL;
+}();
