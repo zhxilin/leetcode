c5923290f3d8e51f924c15e1df26dee1e983ca81
zhxilin <zhxilin@live.com>
2020-09-04 08:06:47 +0800

    1035. Uncrossed Lines

---
+++ array/uncrossed-lines.cpp
@@ -0,0 +1,26 @@
+//1035. Uncrossed Lines
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& A, vector<int>& B) {
+        int m = A.size(), n = B.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+        
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (A[i - 1] == B[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        
+        return dp[m][n];
+    }
+};
+
+static auto _ = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    return 0;
+}();
