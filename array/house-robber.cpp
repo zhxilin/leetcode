dc4f79b385a88c2b28588eeaa84a71001dc16424
zhxilin <zhxilin@live.com>
2021-02-26 17:54:32 +0800

    198. House Robber

--- array/house-robber.cpp
+++ array/house-robber.cpp
@@ -2,16 +2,25 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if (nums.empty()) return 0;
+        // if (nums.empty()) return 0;
         
+        // int n = nums.size();
+        // vector<int> dp(n);
+        
+        // for(int i = 0; i < n; ++i) {
+        //     dp[i] = max(nums[i] + ((i >= 2) ? dp[i - 2] : 0), (i >= 1) ? dp[i - 1] : 0);
+        // }
+        
+        // return dp[n - 1];
+
         int n = nums.size();
-        vector<int> dp(n);
+        vector<int> dp(n + 2);
         
-        for(int i = 0; i < n; ++i) {
-            dp[i] = max(nums[i] + ((i >= 2) ? dp[i - 2] : 0), (i >= 1) ? dp[i - 1] : 0);
+        for (int i = n - 1; i >= 0; --i) {
+            dp[i] = max(dp[i + 1], nums[i] + dp[i + 2]);
         }
         
-        return dp[n - 1];
+        return dp[0];
     }
 };
 
