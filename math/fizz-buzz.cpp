1b8ba7e56754e631a87b97383bfb5530fa6cdfb1
zhxilin <zhxilin@live.com>
2021-03-31 08:53:09 +0800

    412. Fizz Buzz

---
+++ math/fizz-buzz.cpp
@@ -0,0 +1,27 @@
+//412. Fizz Buzz
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> result;
+        
+        for (int i = 1; i <= n; ++i) {
+            if (i % 15 == 0)
+                result.push_back("FizzBuzz");
+            else if (i % 5 == 0)
+                result.push_back("Buzz");
+            else if (i % 3 == 0)
+                result.push_back("Fizz");
+            else
+                result.push_back(to_string(i));
+            
+        }
+        return result;
+    }
+};
+
+static auto _ = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    return 0;
+}();
