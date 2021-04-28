4149759dfc14d27be572ed0911c6ac86f2a7ff7d
zhxilin <zhxilin@live.com>
2021-04-28 09:01:16 +0800

    860. Lemonade Change

---
+++ array/lemonade-change.cpp
@@ -0,0 +1,34 @@
+//860. Lemonade Change
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five = 0, ten = 0;
+        for (int bill : bills) {
+            if (5 == bill)
+                five++;
+            else if (10 == bill) {
+                if (0 == five)
+                    return false;
+                ten++;
+                five--;
+            } else {
+                if (five > 0 && ten > 0) {
+                    five--;
+                    ten--;
+                } else if (five >= 3) 
+                    five -= 3;
+                else
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+};
+
+static auto _ = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    return 0;
+}();
