90cf80b170a444e1db44beff5607ced7b85a0803
zhxilin <zhxilin@live.com>
2021-05-19 09:08:54 +0800

    1237. Find Positive Integer Solution for a Given Equation

---
+++ math/find-positive-integer-solution-for-a-given-equation.cpp
@@ -0,0 +1,37 @@
+//1237. Find Positive Integer Solution for a Given Equation
+/*
+ * // This is the custom function interface.
+ * // You should not implement it, or speculate about its implementation
+ * class CustomFunction {
+ * public:
+ *     // Returns f(x, y) for any given positive integers x and y.
+ *     // Note that f(x, y) is increasing with respect to both x and y.
+ *     // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+ *     int f(int x, int y);
+ * };
+ */
+
+class Solution {
+public:
+    vector<vector<int>> findSolution(CustomFunction& customfunction, int z) {
+        vector<vector<int>> result;
+        for (int x = 1; x <= 1000; ++x) {
+            for (int y = 1; y <= 1000; ++y) {
+                int v = customfunction.f(x, y);
+                if (v == z)
+                    result.push_back({x, y});
+                else if(v > z) 
+                    break;
+            }
+        }
+        
+        return result;
+    }
+};
+
+static auto _ = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    return 0;
+}();
