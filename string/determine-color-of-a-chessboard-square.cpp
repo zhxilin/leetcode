8cb49ee4d401c674d76242bb065cfcbbd2200044
zhxilin <zhxilin@live.com>
2021-06-09 08:19:58 +0800

    1812. Determine Color of a Chessboard Square

---
+++ string/determine-color-of-a-chessboard-square.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool squareIsWhite(string coordinates) {
+        int x = coordinates[0] - 'a';
+        int y = coordinates[1] - '0';
+        
+        if (x % 2 == 0)
+            return y % 2 == 0;
+        
+        return y % 2 != 0;
+    }
+};
